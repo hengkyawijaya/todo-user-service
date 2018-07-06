78891383549c935249f188d5c225efa7464234b9
Hengky Anang Wijaya <hengky.anang.w@mail.ugm.ac.id>
2018-07-06 17:50:55 +0700

    check auth get

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -3,15 +3,13 @@ const axios = require("axios");
 module.exports = async (req, res, next) => {
   const { authorization } = req.headers
   try {
-    const theUser = await axios.post("http://todo-auth-service:8001/auth/check", {}, {
+    const theUser = await axios.get("http://todo-auth-service:8001/auth/check", {
       headers : {
         authorization
       }
     });
 
-    console.log(theUser);
-
-    return next({
+     next({
       auth: theUser.data.data.user
     })
   } catch(err) {
