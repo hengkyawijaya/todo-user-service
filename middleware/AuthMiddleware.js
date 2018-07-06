7f9261f448ddbdab32755d17db64047256fc260e
Hengky Anang Wijaya <hengky.anang.w@mail.ugm.ac.id>
2018-07-06 17:06:59 +0700

    add header auth

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -1,8 +1,13 @@
 const axios = require("axios");
 
 module.exports = async (req, res, next) => {
+  const { authorization } = req.headers
   try {
-    const theUser = await axios.post("http://todo-auth-service:8001/auth/check", {});
+    const theUser = await axios.post("http://todo-auth-service:8001/auth/check", {}, {
+      headers : {
+        authorization
+      }
+    });
 
     return next({
       auth: theUser.data
